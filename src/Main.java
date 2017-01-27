b8ace80dca8cf6068e6a5d3cc51adccb6d79bb9c
mdbubb <mdb17@thebubbs.net>
2017-01-26 19:17:58 -0500

    RGB to Hex done

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,13 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter and Color name (1), a Hex code (2), or an RGB triplet (3)");
         String pos = input.next();
-        if(pos.equalsIgnoreCase("1")){
+        if (pos.equalsIgnoreCase("1")) {
 
-        }
-        else if (pos.equalsIgnoreCase("2")){
+        } else if (pos.equalsIgnoreCase("2")) {
 
-        }
-        else if (pos.equalsIgnoreCase("3")){
-            System.out.println("Enter the red value");
-           hex h = new hex();
-            hex2 d = new hex2();
+        } else if (pos.equalsIgnoreCase("3")) {
+            hex h = new hex();
             String red = h.RGBtoHex();
-                    red = d.nextMethod();
         }
     }
 }
--- src/hex.java
+++ src/hex.java
@@ -8,13 +8,14 @@ public class hex {
     public String RGBtoHex() {
         String color;
         color = color1;
-        for(int i=0; i<=2; i++) {
-            if (i == 0) {
+        hex2 h = new hex2();
+        for (j = 0; j <= 2; j++) {
+            if (j == 0) {
                 color1 = "Red";
             }
-            if (i == 1) {
+            if (j == 1) {
                 color1 = "Green";
-            } else if (i == 2) {
+            } else if (j == 2) {
                 color1 = "Blue";
             }
 
@@ -25,37 +26,55 @@ public class hex {
             hi = input.nextInt();
             if (hi > 0 && hi <= 255) {
                 int goesInto = hi / 16;
+
                 //int rem = 16 % hi;
                 // System.out.println(rem);
                 if (goesInto < 10) {
-                    for (int j = 0; i < 10; i++) {
+                    for (int j = 0; j < 10; j++) {
                         if (goesInto == j) {
                             System.out.println(j);
+                            h.nextMethod();
+
                         }
                     }
 
                 }
 
+
                 if (goesInto == 10) {
                     System.out.println("A");
+                    h.nextMethod();
+
                 } else if (goesInto == 11) {
                     System.out.println("B");
+                    h.nextMethod();
+
                 } else if (goesInto == 12) {
                     System.out.println("C");
+                    h.nextMethod();
+
                 } else if (goesInto == 13) {
                     System.out.println("D");
+                    h.nextMethod();
+
                 } else if (goesInto == 14) {
                     System.out.println("E");
+                    h.nextMethod();
+
                 } else if (goesInto == 15) {
                     System.out.println("F");
+                    h.nextMethod();
 
                 }
             }
         }
 
-return  "hi"   ; }
+
+        return "hi";
+    }
 
 
-public static int hi;
-public static String color1;
+    public static int hi;
+    public static String color1;
+    public static int j;
 }
