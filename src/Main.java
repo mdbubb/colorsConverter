5b2ab1ca473bd5a1d90e8c5804c835614e1e8b80
mdbubb <mdb17@thebubbs.net>
2017-01-26 20:17:27 -0500

    Close

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter and Color name (1), a Hex code (2), or an RGB triplet (3)");
         String pos = input.next();
         if (pos.equalsIgnoreCase("1")) {
+            CommonName c = new CommonName();
+            System.out.println("Enter any common HTML name");
+            String res = input.next();
+            c.nextMethod(res);
+
+
 
         } else if (pos.equalsIgnoreCase("2")) {
+            CommonName code = new CommonName();
+            System.out.println("Enter your Hexidecimal code");
+            String code1 = input.next();
+            code.almostDone(code1);
 
         } else if (pos.equalsIgnoreCase("3")) {
             hex h = new hex();
